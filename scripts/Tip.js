46b2e954ac3f70643bb45b821c171682629e466a
Travis Milner <travismilner1@gmail.com>
2020-10-08 09:47:58 -0500

    added tip js and made function with tips

---
+++ scripts/Tip.js
@@ -0,0 +1,7 @@
+export const tips = (tip) => {
+    return `
+        <p>${tip.name} and ${tip.detail}</p>
+        <p>${tip.name} and ${tip.detail}</p>
+        <p>${tip.name} and ${tip.detail}</p>
+        `
+}
