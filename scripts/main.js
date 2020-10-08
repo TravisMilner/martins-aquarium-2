c779e3c3369de113a0e5441cbf676e7c9befa1c4
Travis Milner <travismilner1@gmail.com>
2020-10-08 11:01:36 -0500

    added the tips and linked it all together

--- scripts/main.js
+++ scripts/main.js
@@ -1,20 +1,19 @@
 import { useFish } from "./FishDataProvider.js"
 import { FishList} from "./FishList.js"
-import { useTips} from "./TipsDataProvider.js"
+import { useTips } from "./TipsDataProvider.js"
+import { TipList } from "./TipList.js"
 const allTheFish = useFish()
+const allTheTips = useTips()
 
 for (const fish of allTheFish) {
     console.log(fish)
 }
-
+for (const tips of allTheTips) {
+    console.log(tips);
+}
 
 
 
 FishList();
 useTips();
-
-const allTheTips = useTips();
-
-for (const tip of allTheTips) {
-    console.log(tip);
-}
+TipList();
